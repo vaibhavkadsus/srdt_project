9b2a77b46903be4ec38b82ac5d77276b1bdfac62
vaibhav <vaibhavkadus3131@gmail.com>
2025-06-13 18:39:03 +0530

    home updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,11 +3,11 @@ import photo1 from "../assets/Home/photo1.jpeg";
 import photo2 from "../assets/Home/photo2.jpeg";
 import photo3 from "../assets/Home/photo3.jpeg";
 import photo4 from "../assets/Home/photo4.jpeg";
-import photo4 from "../assets/Home/photo5.jpeg";
-import photo4 from "../assets/Home/photo6.jpeg";
+import photo5 from "../assets/Home/photo5.jpeg";
+import photo6 from "../assets/Home/photo6.jpeg";
 import "./Home.css";
 
-const images = [photo1, photo2, photo3, photo4];
+const images = [photo1, photo2, photo3, photo4, photo5, photo6];
 
 const Home = () => {
   const [current, setCurrent] = useState(0);
@@ -15,11 +15,11 @@ const Home = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % images.length);
-    }, 4000); // 4 seconds
+    }, 4000); 
 
     return () => clearInterval(interval);
   }, []);
-
+ 
   return (
     <div className="slideshow">
       {images.map((img, index) => (
