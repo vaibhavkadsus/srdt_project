79fc6d6df68168959528e6ab7b5563ee23a58273
vaibhav <vaibhavkadus3131@gmail.com>
2025-06-13 16:58:14 +0530

    home update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import photo1 from "../assets/Home/photo1.jpeg";
+import photo2 from "../assets/Home/photo2.jpeg";
+import photo3 from "../assets/Home/photo3.jpeg";
+import photo4 from "../assets/Home/photo4.jpeg";
+import photo4 from "../assets/Home/photo5.jpeg";
+import photo4 from "../assets/Home/photo6.jpeg";
+import "./Home.css";
 
-function Home() {
+const images = [photo1, photo2, photo3, photo4];
+
+const Home = () => {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 4000); // 4 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="slideshow">
+      {images.map((img, index) => (
+        <img
+          key={index}
+          src={img}
+          alt={`slide-${index}`}
+          className={`slide ${index === current ? "active" : ""}`}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import "./Navbar.css";
-import logo from './assets/Logo.png';
-import { Link } from 'react-router-dom';
+import logo from "./assets/Logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <img src={logo} alt="Logo" className="logo" />
-                <div className="slogan">
-                    <h1 className="srdt">
-                        <span className="s">S</span>
-                        <span className="r">R</span>
-                        <span className="d">D</span>
-                        <span className="t">T</span>
-                    </h1>
-                    <p className="tagline">------ A Step Towards Social Change -----</p>
-                </div>
-            </div>
-            <ul className="navbar-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/donate">Donate</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/gallary">Gallery</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="slogan">
+          <h1 className="srdt">
+            <span className="s">S</span>
+            <span className="r">R</span>
+            <span className="d">D</span>
+            <span className="t">T</span>
+          </h1>
+          <p className="tagline">------ A Step Towards Social Change -----</p>
+        </div>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/donate">Donate</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/gallary">Gallery</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
