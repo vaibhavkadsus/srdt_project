c1927d0dc85e62efc4e47b0446ad95f5af288d4c
vaibhav <vaibhavkadus3131@gmail.com>
2025-06-13 11:15:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
