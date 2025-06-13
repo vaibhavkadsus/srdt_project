3273ace17a6f267b246c7e64ff3497998da89ac1
Abhishek Mahajan <abhishekmahajan@gtasterix.com>
2025-06-13 11:24:34 +0530

    navbar update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">SRDT</a>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,34 +22,23 @@ function Navbar() {
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+               <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">About</a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+               <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Projects</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+               <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Gallary</a>
               </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+               <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Blog</a>
+              </li>
+               <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Contact</a>
+              </li>
+           </ul>
+           
           </div>
         </div>
       </nav>
