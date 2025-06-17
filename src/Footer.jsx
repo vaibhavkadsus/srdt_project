c50b5296017e15f739a0b9648c016022ffba48e6
Abhishek Mahajan <abhishekmahajan@gtasterix.com>
2025-06-17 16:29:18 +0530

    footer update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -36,9 +36,9 @@ const Footer = () => {
         <div className="footer-section social">
           <h4>Follow Us</h4>
           <div className="social-icons">
-            <a href="#"><FaFacebookF /></a>
-            <a href="#"><FaInstagram /></a>
-            <a href="#"><FaLinkedinIn /></a>
+            <a href="https://www.facebook.com/profile.php?id=100087210614157&mibextid=LQQJ4d"><FaFacebookF /></a>
+            <a href="https://www.instagram.com/sai_rural_development_trust_?igsh=MWxpM3VvNHpzc3p0Nw=="><FaInstagram /></a>
+            <a href="https://www.linkedin.com/company/sai-rural-development-trust/"><FaLinkedinIn /></a>
           </div>
         </div>
       </div>
